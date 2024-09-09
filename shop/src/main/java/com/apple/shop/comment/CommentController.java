09af591034995c107f2717b3c6a399b10cf95a89
fu11-M <132278765+fu11-M@users.noreply.github.com>
2024-09-09 20:37:10 +0900

    댓글 기능 구현

--- shop/src/main/java/com/apple/shop/comment/CommentController.java
+++ shop/src/main/java/com/apple/shop/comment/CommentController.java
@@ -3,27 +3,40 @@ package com.apple.shop.comment;
 import com.apple.shop.CustomUser;
 import com.apple.shop.comment.Comment;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentRepository commentRepository;
-
     @PostMapping("/comment")
     String postComment(@RequestParam String content,
                        @RequestParam Integer parent,
                        Authentication authentication){
-        CustomUser CustomUser = (CustomUser) authentication.getPrincipal();
+        CustomUser customUser = (CustomUser) authentication.getPrincipal();
 
         var data = new Comment();
         data.setComment(content);
-        data.setUsername(CustomUser.getUsername());
+        data.setUsername(customUser.getUsername());
+        System.out.println(customUser.getUsername());
         data.setParentId(parent);
         commentRepository.save(data);
-        return "";
+        return "redirect:/list";
         }
+
+
+    @GetMapping("/comments")
+    String getComments(@RequestParam Integer parentId, Model model) {
+        List<Comment> comments = commentRepository.findAllByParentId(parentId);
+        model.addAttribute("comments", comments);
+        return "detail.html";
+    }
 }
